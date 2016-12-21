f5a2afa54055cf252f3fb4ca1f49ed8ce58e87e5
Leo Andeol <leo.andeol@gmail.com>
2016-12-21 17:47:55 +0100

    Updating the docs

--- docs/search/functions_8.js
+++ docs/search/functions_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['partial_5fminimum_5ftree',['partial_minimum_tree',['../classNetwork.html#a245b6ac9f8fca8e0bfbf0597f1e07172',1,'Network']]]
+  ['partial_5fminimum_5ftree',['partial_minimum_tree',['../classNetwork.html#a245b6ac9f8fca8e0bfbf0597f1e07172',1,'Network']]],
+  ['partial_5ftree',['partial_tree',['../classInterface.html#ac438ac9da54d0fd957624bbc69f8ab6f',1,'Interface']]]
 ];
