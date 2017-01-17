772c73cb35024b3d0f6d0b68d14e4936f611aa37
Leo Andeol <leo.andeol@gmail.com>
2017-01-17 19:40:04 +0100

    Updating docs

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['add_5fcable',['add_cable',['../classNetwork.html#a16b4fe4c6d98f3c4d81753a8ea84f5b2',1,'Network']]],
   ['add_5fpath',['add_path',['../classNetwork.html#a83414253632467907bd5c28037ca2e3e',1,'Network']]],
-  ['add_5frouteur',['add_routeur',['../classNetwork.html#a181eb716b492eb654282782841de33ec',1,'Network::add_routeur()'],['../classNetwork.html#a8254d70880b2ca3634ec310eec835475',1,'Network::add_routeur(std::string &amp;name)']]]
+  ['add_5frouteur',['add_routeur',['../classNetwork.html#a181eb716b492eb654282782841de33ec',1,'Network::add_routeur()'],['../classNetwork.html#a6e69479e21762f5ce13e5d9c22ff26a8',1,'Network::add_routeur(std::string &amp;name, std::string isMC=&quot;true&quot;)']]]
 ];
